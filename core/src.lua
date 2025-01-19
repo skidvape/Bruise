888db6c2d2a5bd42075d1d10f980b73769eb5df3
sstvskids <staavstudios@gmail.com>
2025-01-19 20:20:18 +1100

    Update src.lua

--- core/src.lua
+++ core/src.lua
@@ -1,5 +1,5 @@
 -- init
-local uilib = loadstring(game:HttpGet('https://github.com/depthso/Roblox-ImGUI/raw/main/ImGui.lua'))();
+local uilib = loadstring(game:HttpGet("https://github.com/biggaboy212/Maclib/releases/latest/download/maclib.txt"))()
 
 -- services
 local cloneref = cloneref or function(v) return v; end;
@@ -17,76 +17,6 @@ local lplr = playersService.LocalPlayer;
 
 -- functions/tables
 local swordmeta = loadstring(game:HttpGet('https://github.com/skidvape/Bruise/raw/main/core/meta.lua'))();
-local ui = {
-    Window = uilib:CreateWindow({
-        Title = "Bruise",
-        Size = UDim2.new(0, 350, 0, 370),
-        Position = UDim2.new(0.5, 0, 0, 70)
-    });
-    tabs = {
-        ConsoleTab = ui.Window:CreateTab({
-            Name = "Console"
-        })
-    };
-};
-
---// console
-
-ui.Window:Center();
-
-ui.Window:ShowTab(ui.tabs.ConsoleTab) 
-        
-local Row2 = ui.tabs.ConsoleTab:Row()
-
-ui.tabs.ConsoleTab:Separator({
-    Text = "Console Example:"
-})
-
-local Console = ui.tabsConsoleTab:Console({
-    Text = "Console example",
-    ReadOnly = true,
-    LineNumbers = false,
-    Border = false,
-    Fill = true,
-    Enabled = true,
-    AutoScroll = true,
-    RichText = true,
-    MaxLines = 50
-})
-
-Row2:Button({
-    Text = "Clear",
-    Callback = Console.Clear
-})
-Row2:Button({
-    Text = "Copy"
-})
-Row2:Button({
-    Text = "Pause",
-    Callback = function(self)
-        local Paused = shared.Pause
-        Paused = not (Paused or false)
-        shared.Pause = Paused
-        
-        self.Text = Paused and "Paused" or "Pause"
-        Console.Enabled = not Paused
-    end,
-})
-Row2:Fill()
-
-coroutine.wrap(function()
-    while wait() do
-        local Date = DateTime.now():FormatLocalTime("h:mm:ss A", "en-us")
-        
-        Console:AppendText(
-            `<font color="rgb(240, 40, 10)">[Random Math]</font>`, 
-            math.random()
-        )
-        Console:AppendText(
-            `[{Date}] {Console}`
-        )
-    end
-end)()
 
 run = function(v)
     local suc, res = pcall(function()
@@ -95,3 +25,91 @@ run = function(v)
     
     if res then writefile('errorlog.txt', tostring(res)); end;
 end;
+
+local Window = uilib:Window({
+	Title = "Bruise",
+	Subtitle = "A free, fully open-source solara supported script for Roblox Bedwars.",
+	Size = UDim2.fromOffset(868, 650),
+	DragStyle = 1,
+	DisabledWindowControls = {},
+	ShowUserInfo = true,
+	Keybind = Enum.KeyCode.RightShift,
+	AcrylicBlur = true,
+})
+
+local globalSettings = {
+	UIBlurToggle = Window:GlobalSetting({
+		Name = "UI Blur",
+		Default = Window:GetAcrylicBlurState(),
+		Callback = function(bool)
+			Window:SetAcrylicBlurState(bool)
+			Window:Notify({
+				Title = Window.Settings.Title,
+				Description = (bool and "Enabled" or "Disabled") .. " UI Blur",
+				Lifetime = 5
+			})
+		end,
+	}),
+	NotificationToggler = Window:GlobalSetting({
+		Name = "Notifications",
+		Default = Window:GetNotificationsState(),
+		Callback = function(bool)
+			Window:SetNotificationsState(bool)
+			Window:Notify({
+				Title = Window.Settings.Title,
+				Description = (bool and "Enabled" or "Disabled") .. " Notifications",
+				Lifetime = 5
+			})
+		end,
+	}),
+	ShowUserInfo = Window:GlobalSetting({
+		Name = "Show User Info",
+		Default = Window:GetUserInfoState(),
+		Callback = function(bool)
+			Window:SetUserInfoState(bool)
+			Window:Notify({
+				Title = Window.Settings.Title,
+				Description = (bool and "Showing" or "Redacted") .. " User Info",
+				Lifetime = 5
+			})
+		end,
+	})
+};
+
+local tabs = {
+	modules = Window:TabGroup(),
+    configs = Window:TabGroup()
+}
+
+local uitabs = {
+    combat = tabs.modules:Tab({
+        Name = "Combat",
+        Image = "rbxassetid://14368312652"
+    }),
+    movement = tabs.modules:Tab({
+        Name = "Movement",
+        Image = "rbxassetid://14368306745"
+    }),
+    render = tabs.modules:Tab({
+        Name = "Render",
+        Image = "rbxassetid://14368350193"
+    }),
+    utility = tabs.modules:Tab({
+        Name = "Utility",
+        Image = "rbxassetid://14368359107"
+    }),
+    settings = tabs.configs:Tab({
+        Name = "Config Settings",
+        Image = "rbxassetid://10734950309"
+    })
+};
+
+uilib:SetFolder("Maclib")
+uitabs.settings:InsertConfigSection("Left")
+
+Window.onUnloaded(function()
+	print("Unloaded!")
+end)
+
+for i,v in ipairs(uitabs) do v:Select() end;
+uilib:LoadAutoLoadConfig()
