4ce245f08a8a08004c15b2effad9129a81f8a893
sstvskids <staavstudios@gmail.com>
2025-01-20 15:52:11 +1100

    Update installer.lua

--- core/installer.lua
+++ core/installer.lua
@@ -1,33 +1,21 @@
-local url = 'https://raw.githubusercontent.com/skidvape/Bruise/main';
-
 for _, v in {'bruise', 'bruise/core', 'bruise/songs', 'bruise/core/configs', 'bruise/core/libs', 'bruise/core/games'} do
     if not isfolder(v) then makefolder(v); end;
 end;
 
-local files = {
-    '/core/installer.lua',
-    '/core/libs/meta.lua',
-    '/games/bedwars.lua',
-    '/games/arsenal.lua',
-    '/loader.lua',
-    '/songs/numb.mp3',
-    '/songs/w4ytoof4r.mp3'
-};
-
 local suc, res = pcall(function()
-    for _, v in ipairs(files) do
+    for _, v in {'/core/installer.lua', '/core/libs/meta.lua', '/games/bedwars.lua', '/games/arsenal.lua', '/loader.lua', '/songs/numb.mp3', '/songs/w4ytoof4r.mp3'} do
         if not isfile('Bruise'..v) then
-            writefile('Bruise'..v, game:HttpGet(url..v));
+            writefile('Bruise'..v, game:HttpGet('https://raw.githubusercontent.com/skidvape/Bruise/main'..v));
         elseif isfile('Bruise'..v) then
             delfile('Bruise'..v);
-            writefile('Bruise'..v, game:HttpGet(url..v));
+            writefile('Bruise'..v, game:HttpGet('https://raw.githubusercontent.com/skidvape/Bruise/main'..v));
         end;
     end;
 end);
 
 if res and not suc then writefile('errorlog.lua', tostring(res)); end;
 if game.PlaceId == 286090429 then
-    return loadstring(readfile('bruise/games/arsenal.lua'))();
+    return loadfile('bruise/games/arsenal.lua')();
 else
-    return loadstring(readfile('bruise/games/bedwars.lua'))();
+    return loadfile('bruise/games/bedwars.lua')();
 end;
