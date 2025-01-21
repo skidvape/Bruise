aa85067e3d0f288802e3c23c79a66b0b78ec3f18
sstvskids <staavstudios@gmail.com>
2025-01-21 19:18:13 +1100

    removed libs

--- core/installer.lua
+++ core/installer.lua
@@ -1,9 +1,9 @@
-for _, v in {'bruise', 'bruise/core', 'bruise/songs', 'bruise/core/configs', 'bruise/core/libs', 'bruise/games'} do
+for _, v in {'bruise', 'bruise/core', 'bruise/songs', 'bruise/core/configs', 'bruise/games'} do
     if not isfolder(v) then makefolder(v); end;
 end;
 
 local suc, res = pcall(function()
-    for _, v in {'/core/installer.lua', '/core/libs/meta.lua', '/games/universal.lua', '/games/arsenal.lua', '/games/fisch.lua', '/loader.lua', '/songs/numb.mp3', '/songs/w4ytoof4r.mp3'} do
+    for _, v in {'/core/installer.lua', '/games/universal.lua', '/games/arsenal.lua', '/games/fisch.lua', '/loader.lua', '/songs/numb.mp3', '/songs/w4ytoof4r.mp3'} do
         if not isfile('Bruise'..v) then
             writefile('Bruise'..v, game:HttpGet('https://raw.githubusercontent.com/skidvape/Bruise/main'..v));
         elseif isfile('Bruise'..v) then
