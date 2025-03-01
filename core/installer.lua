2d8424684374f0a6140e0967c642307ba9865d4e
sstvskids <staavstudios@gmail.com>
2025-03-02 09:51:26 +1100

    rewrote installer

--- core/installer.lua
+++ core/installer.lua
@@ -1,35 +1,33 @@
-for _, v in {'bruise', 'bruise/core', 'bruise/core/ui', 'bruise/songs', 'bruise/core/configs', 'bruise/games', 'bruise/errors'} do
-    if not isfolder(v) then makefolder(v); end;
-end;
+local folders = {'bruise', 'bruise/core', 'bruise/core/ui', 'bruise/songs', 'bruise/core/configs', 'bruise/games'}
+local HttpService = game:GetService('HttpService')
 
-local res, err = pcall(function()
-    for _, v in {'/core/installer.lua', '/core/ui/interface.lua', '/games/universal.lua', '/games/arsenal.lua', '/games/fisch.lua', '/loader.lua', '/songs/numb.mp3', '/songs/w4ytoof4r.mp3', '/songs/drift.mp3'} do
-        if not isfile('bruise'..v) then
-            writefile('bruise'..v, game:HttpGet('https://raw.githubusercontent.com/skidvape/Bruise/main'..v));
-        elseif isfile('bruise'..v) then
-            delfile('bruise'..v);
-            writefile('bruise'..v, game:HttpGet('https://raw.githubusercontent.com/skidvape/Bruise/main'..v));
-        end;
-    end;
-end);
+local suc, res = pcall(function()
+    for _, v in pairs(folders) do
+        local url = game:HttpGet('https://api.github.com/repos/skidvape/bruise/contents/'..v)
+        local jsonurl = HttpService:JSONDecode(url)
+        for _, v in jsonurl do
+            if v.type == 'file' then
+                if not isfile(v.name) then
+                    writefile(v.name, game:HttpGet(v.download_url))
+                elseif isfile(v.name) then
+                    delfile(v.name)
+                    writefile(v.name, game:HttpGet(v.download_url))
+                end
+            elseif v.type == 'dir' then
+                if not isfolder(v.name) then makefolder(v.name) elseif isfolder(v.name) then delfolder(v.name) makefolder(v.name) end
+            end
+        end
+    end
+end)
 
-if err then
-    if not isfile('bruise/errors/errorlog.lua') then
-        warn(tostring(err));
-        writefile('bruise/errors/errorlog.lua', debug.traceback(tostring(err)));
-    elseif isfile('bruise/errors/errorlog.lua') then
-        delfile('bruise/errors/errorlog.lua');
-        warn(tostring(err));
-        writefile('bruise/errors/errorlog.lua', debug.traceback(tostring(err)));
-    end;
-end;
-
-if res then
+if suc then
     if game.PlaceId == 286090429 then
-        return loadfile('bruise/games/arsenal.lua')();
+        return loadfile('bruise/games/arsenal.lua')()
     elseif game.PlaceId == 16732694052 then
-        return loadfile('bruise/games/fisch.lua')();
+        return loadfile('bruise/games/fisch.lua')()
     else
-        return loadfile('bruise/games/universal.lua')();
-    end;
-end;
+        return loadfile('bruise/games/universal.lua')()
+    end
+elseif res then
+    warn(res)
+end
