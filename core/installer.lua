7d034dec43b47f893e2ac7c5e4dfc541913d7c32
sstvskids <staavstudios@gmail.com>
2025-03-02 19:01:40 +1100

    Update installer.lua

--- core/installer.lua
+++ core/installer.lua
@@ -1,21 +1,18 @@
-local folders = {'bruise', 'bruise/core', 'bruise/core/ui', 'bruise/songs', 'bruise/core/configs', 'bruise/games', 'bruise/errors'}
 local HttpService = game:GetService('HttpService')
 
 local suc, res = pcall(function()
-    for _, v in pairs(folders) do
-        local url = game:HttpGet('https://api.github.com/repos/skidvape/bruise/contents/'..v)
-        local jsonurl = HttpService:JSONDecode(url)
-        for _, v in jsonurl do
-            if v.type == 'file' then
-                if not isfile(v.name) then
-                    writefile(v.name, game:HttpGet(v.download_url))
-                elseif isfile(v.name) then
-                    delfile(v.name)
-                    writefile(v.name, game:HttpGet(v.download_url))
-                end
-            elseif v.type == 'dir' then
-                if not isfolder(v.name) then makefolder(v.name) elseif isfolder(v.name) then delfolder(v.name) makefolder(v.name) end
+    local url = game:HttpGet('https://api.github.com/repos/skidvape/bruise/contents/'..v)
+    local jsonurl = HttpService:JSONDecode(url)
+    for _, v in pairs(jsonurl) do
+        if v.type == 'file' then
+            if not isfile(v.name) then
+                writefile(v.name, game:HttpGet(v.download_url))
+            elseif isfile(v.name) then
+                delfile(v.name)
+                writefile(v.name, game:HttpGet(v.download_url))
             end
+        elseif v.type == 'dir' then
+            if not isfolder(v.name) then makefolder(v.name) elseif isfolder(v.name) then delfolder(v.name) makefolder(v.name) end
         end
     end
 end)
